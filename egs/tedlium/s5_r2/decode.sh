e2c70c0ee1110b467409450a450338f3dcb3a388
Wang Jian <wantee.wang@gmail.com>
2017-04-27 14:04:52 +0800

    decode with connlm

--- egs/tedlium/s5_r2/decode.sh
+++ egs/tedlium/s5_r2/decode.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-local/chain/run_tdnn.sh --stage 20 --nnet3-affix "" --train-set train --tdnn-affix 1b --decode-nj 4
+local/chain/run_tdnn.sh --stage 19 --nnet3-affix "" --train-set train --tdnn-affix 1b --decode-nj 4
---
+++ egs/tedlium/s5_r2/format_connlm.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+gunzip -c data/lang/g.txt.gz | \
+   utils/remove_oovs.pl data/lang/oov.txt | \
+   utils/phi2disambig.pl | utils/s2eps.pl | \
+   fstcompile --isymbols=data/lang/words.txt --osymbols=data/lang/words.txt | \
+   fstrmepslocal | fstarcsort --sort_type=ilabel > data/lang/G.fst
+echo  "$0: Checking how stochastic G is (the first of these numbers should be small):"
+fstisstochastic data/lang/G.fst || true
+utils/validate_lang.pl --skip-determinization-check data/lang

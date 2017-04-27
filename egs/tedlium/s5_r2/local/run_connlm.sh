e834919bcf83907112c828f37dab71919a5ca467
Wang Jian <wantee.wang@gmail.com>
2017-04-28 00:07:04 +0800

    add connlm rescore scripts

---
+++ egs/tedlium/s5_r2/local/run_connlm.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Begin configuration section.
+num_threads=8 # set this value to the number of physical cores on your CPU
+stage=0
+# End configuration section.
+
+echo "$0 $@"  # Print the command line for logging
+
+. path.sh
+. utils/parse_options.sh
+
+set -e
+
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <lm-dir> <model-dir>"
+  echo "e.g.: $0 data/lang_connlm exp/chain/tdnn1b_sp_bi"
+  echo "Main options:"
+  echo "  --stage <int>"
+  exit 1
+fi
+
+s5_dir=`pwd`
+connlm_dir=`readlink -f $1`
+model_dir=`readlink -f $2`
+
+if [ $stage -le 1 ]; then
+  echo "$0: Performing connLM rescoring on decoding results"
+  for dset in dev test; do
+    sourcedir=${model_dir}/decode_${dset}
+    if [ ! -d "$sourcedir" ]; then
+        echo "$0: WARNING cannot find source dir '$sourcedir' to rescore"
+        continue
+    fi
+    resultsdir=${sourcedir}_connlm
+    rm -rf ${resultsdir}_L0.5
+    steps/connlmrescore.sh --skip_scoring false --N 100 0.5 data/lang $connlm_dir data/${dset}_hires $sourcedir ${resultsdir}_L0.5
+    for coef in 0.25 0.75; do
+        rm -rf ${resultsdir}_L${coef}
+        cp -r ${resultsdir}_L0.5 ${resultsdir}_L${coef}
+       steps/connlmrescore.sh --skip_scoring false --N 100 --stage 7 $coef data/lang $connlm_dir data/${dset}_hires $sourcedir ${resultsdir}_L0.5
+    done
+  done
+fi
---
+++ egs/wsj/s5/utils/connlm_compute_scores.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Compute scores from connLM. Modified from utils/connlm_compute_scores.sh
+
+. ./path.sh || exit 1;
+. utils/parse_options.sh
+
+connlm_root=$KALDI_ROOT/tools/connLM
+
+[ ! -d $connlm_root ] && echo connLM not installed && exit 1;
+
+. $connlm_root/egs/steps/path.sh
+
+if [ $# != 4 ]; then
+  echo "Usage: connlm_compute_scores.sh <connlm-dir> <temp-dir> <input-text> <output-scores>"
+  exit 1;
+fi
+
+dir=$1
+tempdir=$2
+text_in=$3
+scores_out=$4
+
+for x in final.clm; do
+  if [ ! -f $dir/$x ]; then
+    echo "connlm_compute_scores.sh: expected file $dir/$x to exist."
+    exit 1;
+  fi
+done
+
+mkdir -p $tempdir
+cat $text_in | awk '{for (x=2;x<=NF;x++) {printf("%s ", $x)} printf("\n");}' >$tempdir/text
+cat $text_in | awk '{print $1}' > $tempdir/ids # e.g. utterance ids.
+
+connlm-eval --print-sent-prob=true --log-file=$tempdir/eval.log \
+            $dir/final.clm $tempdir/text $tempdir/scores
+
+# scores out, with utterance-ids.
+paste $tempdir/ids $tempdir/scores  > $scores_out
